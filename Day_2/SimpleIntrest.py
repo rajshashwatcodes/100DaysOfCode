7c6760fe90a2ca444fa0779a2ab16408a62c36ff
CodeToCore <rajshashwatcodes@gmail.com>
2022-09-18 20:40:20 +0530

    Simple Intrest Calculation

---
+++ Day_2/SimpleIntrest.py
@@ -0,0 +1,7 @@
+prin = int(input("Enter the Principle: "))
+rate = int(input("Enter the rate of intrest: "))
+time = int(input("Enter the time: "))
+
+si = (prin*rate*time)/100
+
+print("Simple Intrest =",si)
