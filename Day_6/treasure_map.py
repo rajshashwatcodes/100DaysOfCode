bcfd1954cfebcc769e479a9d6cac33ba3f6e6b2d
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-09-22 23:27:47 +0530

    A game program to make a treasure map.

---
+++ Day_6/treasure_map.py
@@ -0,0 +1,14 @@
+row1 = ["😀","😘","🥳"]
+row2 = ["😅","🥰","😜"]
+row3 = ["🤓","😉","😇"]
+map = row1, row2 , row3
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+
+row = int(position[0])
+column = int(position[1])
+
+selected_row = map[column-1]
+selected_row[row - 1] = "X"
+
+print(f"{row1}\n{row2}\n{row3}")
