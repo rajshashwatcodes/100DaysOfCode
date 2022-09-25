dc276b5d1dca5a11e0e40083862fc4842eb9375f
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-09-25 19:35:23 +0530

    A program to find factorial of any number.

---
+++ Day_9/factorial.py
@@ -0,0 +1,7 @@
+#Created by Shashwat Raj
+#Program to find out factorial
+num=int(input("Enter a number: "))
+factorial=1
+for i in range(1,num+1):
+    factorial=factorial*i
+print("Factorial: ",factorial)
