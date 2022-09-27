80418b76f21586b2d5497cec784c29195da6c573
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-09-27 23:41:28 +0530

    A program to show the uses of variables.

---
+++ Day_11/use_of_variables.py
@@ -0,0 +1,12 @@
+# Created by Shashwat Raj
+#Program to show the use of variables
+#decleration of Variables as string
+character_name="Shashwat"
+character_age = "20"
+#printing
+print("There once was a boy named " + character_name + ",") #using variable name in the print sstatement 
+print("he was " + character_age + " years old.") #using variable in the ptint statement
+#Changing the value of variable
+character_name="Golu"
+print("He liked being called "+ character_name +",")
+print("and was too cool for " + character_age + " year olds.")
