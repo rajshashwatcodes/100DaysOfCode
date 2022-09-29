08b76e4206674f63a1ab32816a88217e13bd2574
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-09-29 21:35:45 +0530

    A program to find the greater number of any two number.

---
+++ Day_13/greater_number.py
@@ -0,0 +1,8 @@
+#Created by Shashwat Raj
+#Program to find the greater one of two nos
+num1=int(input("Enter the first Number: "))
+num2=int(input("Enter the Second Number: "))
+if num1 > num2:
+    print("Greater nubmer is: ",num1)
+else:
+    print("Greater nubmer is: ",num2)
