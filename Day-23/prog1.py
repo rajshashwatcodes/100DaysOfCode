9d22218e42abaf4bece00083898808113fe478c3
CodeToCore <rajshashwatcodes@gmail.com>
2022-10-08 23:57:40 +0530

    prog1

---
+++ Day-23/prog1.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for i in range(n):
+    a = int(input())
+    if a > 5:
+        print("Greater than 5")
+    elif a < 5:
+        print("Less than 5")
+    else:
+        print("Equal to 5")
