7e35ff5f1de78bb56c327821dfa2c380b51fb4ef
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-10-19 23:54:11 +0530

    A program to find factorials of any number.

---
+++ Day_29/factorials.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class factorial {
+
+    public static void printFactorial(int n) {
+        // loop
+        int facto = 1;
+        for(int i = n; i >= 1; i--) {
+            facto = facto * i;
+        }
+
+        System.out.println("The factorial is: " + facto);
+        return;
+
+    }
+    public static void main(String args[]) {
+        System.out.print("Enter a number to find factorial: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printFactorial(n);
+    }
+}
