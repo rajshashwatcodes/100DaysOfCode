0c27958318e24b15c48639acee5c7db62240e0a3
CodeToCore <rajshashwatcodes@gmail.com>
2022-10-21 23:18:24 +0530

    Update valleyCount.py

--- Day_24/valleyCount.py
+++ Day_24/valleyCount.py
@@ -1,19 +1,5 @@
 #!/bin/python3
-
-import math
 import os
-import random
-import re
-import sys
-
-#
-# Complete the 'countingValleys' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER steps
-#  2. STRING path
-#
 
 def countingValleys(steps, path):
     v = 0
