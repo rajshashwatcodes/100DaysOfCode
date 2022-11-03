0bee4c29ec748fb314471dfb3a9c2314208105d1
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-11-03 19:51:25 +0530

    A program to print even numbers from 9 to 100.

---
+++ Day_43/evenNumbers.java
@@ -0,0 +1,10 @@
+public class evenNumbers {
+    public static void main(String args[]) {
+        System.out.println("To print even numbers between 1 too 100:");
+        for(int i = 9 ; i <= 100 ; i++) {
+            if(i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
