c3384eec5aee4f41d0099069c9fa43978ae74cc2
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-11-05 21:05:46 +0530

    A program to convert character to integer.

---
+++ Day_45/typeConversion.java
@@ -0,0 +1,23 @@
+public class typeConversion {
+    public static void main(String args[]) {
+        char letter = 'a';
+        char letter2 = 'b';
+        char letter3 = 'z';
+        char letter4 = 'A';
+        char letter5 = 'Z';
+        
+        // converting character to integer
+        int num = letter;
+        int num2 = letter2;
+        int num3 = letter3;
+        int num4 = letter4;
+        int num5 = letter5;
+
+        System.out.println(num);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+
+    }
+}
