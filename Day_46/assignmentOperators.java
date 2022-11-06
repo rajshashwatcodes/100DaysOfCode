03bde20093f32dc5309b55f245bce893fada6938
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-11-06 20:35:49 +0530

    A program to assignment operators.

---
+++ Day_46/assignmentOperators.java
@@ -0,0 +1,21 @@
+package day24;
+
+public class assignmentOperator {
+    public static void main(String args[]) {
+        int a = 10;
+        a += 4; //a = a + 4 = 14
+        int b = 20;
+        b -= 7; //b = b- 7 = 13
+        int c = 15;
+        c *= 2; //c = c * 2 = 30
+        int d = 25;
+        d /= 5; //d = d / 5 = 5
+        int e = 30;
+        e %= 3; //e = e % 3 = 0
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
