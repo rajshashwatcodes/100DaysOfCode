a4957463925337973770a26868cd781a4c38bd05
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-11-06 20:10:33 +0530

    Understanding the working of decrement operators.

---
+++ Day_46/decrementUnary.java
@@ -0,0 +1,17 @@
+public class decrementUnary {
+    public static void main(String args[]) {
+        /*  pre decreament "--a" will first change the value of its own variable...here "a" 
+         and then assign it to the other variable */
+        int a = 10;
+        int b = --a;
+        System.out.println(a);
+        System.out.println(b);
+        /* post decreament "a--" will first assign the value to the other variable 
+        and then change it for its own variable...here "x"  */
+        System.out.println("-----------------------------");
+        int x = 20;
+        int y = x--;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
