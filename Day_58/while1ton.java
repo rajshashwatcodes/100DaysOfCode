16c26bf3757a0d3bc7d9cb4e37061f1af92c407e
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-11-18 14:46:54 +0530

    A program to print numbers till the user likes.

---
+++ Day_58/while1ton.java
@@ -0,0 +1,16 @@
+package day31;
+import java.util.*;
+
+public class while1ton {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the last number: ");
+        int n = sc.nextInt();
+        int a = 1;
+        while(a <= n) {
+            System.out.print(a + " ");
+            a++;
+        }
+        sc.close();
+    }
+}
