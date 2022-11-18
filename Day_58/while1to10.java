a11d7e254efb475cbb99dc0d68a7f237a25bd8ae
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-11-18 14:17:01 +0530

    A program using while loop.

---
+++ Day_58/while1to10.java
@@ -0,0 +1,9 @@
+public class while1to10 {
+    public static void main(String args[]) {
+        int counter = 1;
+        while(counter <= 10) {
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
