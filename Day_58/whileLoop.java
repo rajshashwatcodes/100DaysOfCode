0fc229f3e39a591df669a3b3430843075b0e7f70
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-11-18 14:13:25 +0530

    A program using while loop.

---
+++ Day_58/whileLoop.java
@@ -0,0 +1,10 @@
+public class whileLoop {
+    public static void main(String args[]) {
+        int counter = 0;
+        while(counter < 10) {
+            System.out.println("Shashwat Raj");
+            counter++;
+        }
+        System.out.println("Printed name 10 times.");
+    }
+}
