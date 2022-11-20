c37b5c4b573bac68d953b8d49ff919d23181d83f
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-11-20 22:08:42 +0530

    A program to print Hello World 10 times.

---
+++ Day_60/forLoop.java
@@ -0,0 +1,8 @@
+public class forLoop {
+    public static void main(String args[]) {
+        for(int i = 1 ; i <=10 ; i++) {
+            System.out.println("Hello World!");
+        }
+        System.out.println("'Hello world!' is printed 10 times.");
+    }
+}
