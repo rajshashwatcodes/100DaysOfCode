2618ea01b75a4af52a6983e67c16fae7814dc4ff
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-11-26 23:58:03 +0530

    A program to print name using do while loop.

---
+++ Day_65/doWhile2.java
@@ -0,0 +1,11 @@
+public class doWhile {
+    public static void main(String args[]) {
+        int i = 1;
+        do {
+            System.out.println("Shashwat Raj");
+            i++;
+        }
+        while(i <= 50);
+        System.out.println("'Shashwat Raj' printed 50 times");
+    }
+}
