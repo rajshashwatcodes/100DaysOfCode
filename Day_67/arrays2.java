c2a4689804c996dd3cb6100334867f77e025b4de
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-11-29 23:54:44 +0530

    A program understanding arrays.

---
+++ Day_67/arrays2.java
@@ -0,0 +1,18 @@
+public class arrays2 {
+    public static void update(int marks[]) {
+        for(int i = 0; i < marks.length; i++) {
+            marks[i] = marks[i] + 1;
+        }
+    }
+
+    public static void main(String args[]) {
+        int marks[] = {97 , 98 , 99} ;
+        update(marks);
+
+        // print marks
+        for(int i = 0; i <marks.length; i++) {
+            System.out.print(marks[i] + " ");
+        }
+        System.out.println();
+    }
+}
