a9cd6d4d90c42d4ca103cca2629593645fa7f9df
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-12-07 23:25:24 +0530

    a program to make palindrome number.

---
+++ Day_72/palindromeNum.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class palindromeNum {
+    public static void main(String args[]) {
+        System.out.println("Please Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        int palindrome = sc.nextInt();
+
+        if(isPalindrome(palindrome)) {
+            System.out.println("Number: " + palindrome + " is a palindrome");
+        }
+        else {
+            System.out.println("Number: " + palindrome + " is not a palindrome");
+        }
+        sc.close();
+    }
+    public static boolean isPalindrome(int number) {
+        int palindrome = number;
+        int reverse = 0;
+
+        while(palindrome != 0) {
+            int remainder = palindrome % 10;
+            reverse = reverse * 10 + remainder;
+            palindrome = palindrome / 10;
+        }
+        if(number == reverse) {
+            return true;
+        }
+        return false;
+    }
+}
