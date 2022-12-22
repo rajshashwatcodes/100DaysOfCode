36dfe87d29f8bfa955babe56ee349b19b64abcec
Shashwat Raj <113779833+CodeToCore@users.noreply.github.com>
2022-12-22 23:19:56 +0530

    printing number of pairs along with the pairs formed.

--- Day_77/pairsArrays.java
+++ Day_77/pairsArrays.java
@@ -1,13 +1,18 @@
+package day44;
+
 public class pairsArrays {
     public static void pairs(int numbers[]) {
+        int tp = 0; //total number of pairs
         for(int i = 0 ; i < numbers.length ; i++){
             // 2, 4, 6, 8, 10
             int current = numbers[i];
             for(int j = i + 1 ; j < numbers.length ; j++) {
                 System.out.print("(" + current + "," + numbers[j] + ")");
+                tp++;
             }
             System.out.println();
         }
+        System.out.println("Total number of pairs are: " + tp);
 
     }
     public static void main(String args[]) {
