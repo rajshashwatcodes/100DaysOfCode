092ecc77eaacd2d3ef26bf4300ccd147bc85cf19
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-01-12 23:31:02 +0530

    A program to find maximum profit using arrays.

---
+++ Day_84/ArrayQuest.java
@@ -0,0 +1,22 @@
+public class ArrayQuest {
+
+    public static int maxProfit(int prices[]) {
+        int buy = prices[0];
+        int profit = 0;
+        
+        for(int i = 1; i < prices.length; i++) {
+            if(buy < prices[i]) {
+                profit = Math.max(prices[i] - buy, profit);
+            }
+            else {
+                buy = prices[i];
+            }
+        }
+        return profit;
+    }
+
+    public static void main(String args[]) {
+        int prices[] = {7, 1, 5, 3, 6, 4};
+        System.out.println("The maximum profit is: " + maxProfit(prices));
+    }
+}
