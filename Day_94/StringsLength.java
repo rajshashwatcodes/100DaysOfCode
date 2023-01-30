8985096cbe3c4e3b8b8257b438bceaf33fd8e7e5
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-01-30 23:44:07 +0530

    A program to use length function in strings.

---
+++ Day_94/StringsLength.java
@@ -0,0 +1,9 @@
+package day58;
+
+public class StringsLength {
+    public static void main(String args[]) {
+        String fullName = "Tony Stark"; //space will also be counted
+        System.out.println(fullName.length());
+        
+    }
+}
