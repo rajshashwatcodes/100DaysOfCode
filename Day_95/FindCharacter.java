a361024979e5bcc6aa565f9a6dab01c5db893bfe
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-02-02 23:28:02 +0530

    A program to find character using chatAt method.

---
+++ Day_95/FindCharacter.java
@@ -0,0 +1,9 @@
+public class FindCharacter {
+    public static void main(String args[]) {
+        String firstName = "Amisha";
+        String lastName = "Verma";
+        String fullName = firstName + " " + lastName;
+        // using "charAt" keyword to find any character
+        System.out.println(fullName.chatAt(0));
+    }
+}
