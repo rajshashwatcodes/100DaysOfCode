2a15fba7a75cc0cb05073746a2661facd6e2143b
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-02-03 23:35:48 +0530

    Created a function to print every letter of the string.

--- Day_95/FindCharacter.java
+++ Day_95/FindCharacter.java
@@ -1,9 +1,18 @@
 public class FindCharacter {
+
+    public static void printLetters(String str) {
+        for(int i = 0 ; i < str.length() ; i++) {
+            System.out.print(str.charAt(i) + " ");
+        }
+        System.out.println();
+    }
     public static void main(String args[]) {
         String firstName = "Amisha";
         String lastName = "Verma";
         String fullName = firstName + " " + lastName;
         // using "charAt" keyword to find any character
-        System.out.println(fullName.chatAt(0));
+        System.out.println(fullName.charAt(0));
+
+        printLetters(fullName);
     }
 }
