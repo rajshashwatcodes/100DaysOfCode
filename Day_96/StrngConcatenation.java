0803ba822cbbaddc26028eac002abdb1c3345b7e
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-02-08 23:35:43 +0530

    A program to do string concatenation.

---
+++ Day_96/StrngConcatenation.java
@@ -0,0 +1,8 @@
+public class StringConcatenation {
+    public static void main(String args[]) {
+        String firstName = "Amisha";
+        String lastName = "Verma";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+}
