a8e4894a7babc0fe9fa92f097f53216f7ff7c7af
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-02-09 23:29:26 +0530

    A program to reate a 2 D array.

---
+++ Day_97/TwoDArray.java
@@ -0,0 +1,23 @@
+public class TwoDArrays {
+    public static void main(String args[]) {
+        int matrix[][] = new int[3][3];    
+        matrix[0][0] = 1;
+        matrix[0][1] = 2;
+        matrix[0][2] = 3;
+        matrix[1][0] = 4;
+        matrix[1][1] = 5;
+        matrix[1][2] = 6;
+        matrix[2][0] = 7;
+        matrix[2][1] = 8;
+        matrix[2][2] = 9;
+        System.out.println(matrix[0][0]);
+        System.out.println(matrix[0][1]);
+        System.out.println(matrix[0][2]);
+        System.out.println(matrix[1][0]);
+        System.out.println(matrix[1][1]);
+        System.out.println(matrix[1][2]);
+        System.out.println(matrix[2][0]);
+        System.out.println(matrix[2][1]);
+        System.out.println(matrix[2][2]);
+    }
+}
