0e88d6a44698faaea3041a32269d50bef95fcd3c
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-03-18 23:24:23 +0530

    An array program.

---
+++ Day_100/ArrayProgram.java
@@ -0,0 +1,10 @@
+public class ArrayProgram {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println("Array elements are: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
+
