e07c47a7e39755289100f58e1881f5cef02cdff5
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-04-11 23:11:37 +0530

    A program to find the difference in the subarrays formed.

---
+++ Day_101/DifferenceSubarrays.java
@@ -0,0 +1,20 @@
+public class DifferenceSubarrays {
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 1, 2, 4, 3 };
+        int target = 7;
+        int min = Integer.MAX_VALUE;
+        int sum = 0;
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            end = i;
+            while (sum >= target) {
+                min = Math.min(min, end - start + 1);
+                sum -= arr[start];
+                start++;
+            }
+        }
+        System.out.println(min);
+    }
+}
