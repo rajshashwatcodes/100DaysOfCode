e3b28fcd167d6e87f49d22495c0d5ee1fb02f395
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-04-21 23:44:03 +0530

    If else program.

---
+++ Day_9/PassOrFail.java
@@ -0,0 +1,10 @@
+public class PassOrFail {
+    public static void main(String[] args) {
+        int marks = 50;
+        if (marks >= 35) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
