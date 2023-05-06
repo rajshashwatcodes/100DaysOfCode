6ebf7bbc68367079ea0bffe98dbe40106a82534b
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-05-06 23:57:32 +0530

    A while loop program.

---
+++ Day_9/while_loop.py
@@ -0,0 +1,23 @@
+# while loop
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+# while loop with else
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+else:
+    print("Loop is over")
+
+# while loop with break
+i = 1
+while i <= 10:
+    print(i)
+    if i == 5:
+        break
+    i += 1
+
+
