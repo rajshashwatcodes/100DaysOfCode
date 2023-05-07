25d787120c53ce52473d6a70beb069bac17a316c
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-05-07 23:46:28 +0530

    A program to append list.

---
+++ Day_11/append_list.py
@@ -0,0 +1,13 @@
+# create an empty list
+my_list = []
+
+# append items to the list
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+my_list.append(4)
+my_list.append(5)
+
+# print the list
+print(my_list)
+
