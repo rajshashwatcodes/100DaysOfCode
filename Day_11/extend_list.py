d7813897657146b110346e436e9e69fc92ae3f13
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-05-08 23:57:36 +0530

    A program to  extend a list.

---
+++ Day_11/extend_list.py
@@ -0,0 +1,19 @@
+# create an empty list
+my_list = []
+
+# append items to the list
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+my_list.append(4)
+my_list.append(5)
+
+# print the list
+print(my_list)
+
+# extend the list
+my_list.extend([6, 7, 8, 9, 10])
+
+# print the list
+print(my_list)
+
