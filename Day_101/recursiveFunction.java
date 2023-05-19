fc92f4868f06e6470b998394bd1e032f7f9f5362
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-05-20 00:55:20 +0530

    Create recursiveFunction.java

---
+++ Day_101/recursiveFunction.java
@@ -0,0 +1,14 @@
+public class recursiveFunction {
+    public static void main(String[] args) {
+        System.out.println(sum(4));
+    }
+
+    public static int sum(int n) {
+        if(n == 0) {
+            return 0;
+        }
+        else {
+            return n + sum(n - 1);
+        }
+    }
+}
