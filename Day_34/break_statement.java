0856b99bb17d8b81831e1d64f9bbcbcabe735ccd
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-06-20 23:17:40 +0530

    Create break_statement.java

---
+++ Day_34/break_statement.java
@@ -0,0 +1,12 @@
+public class break_statement {
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 10; i++) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
+
