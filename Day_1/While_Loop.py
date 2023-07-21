308672e2bd226f49d48cbbf3d9beee6e24f4e296
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-07-21 23:48:43 +0530

    Create While_Loop.py

---
+++ Day_1/While_Loop.py
@@ -0,0 +1,10 @@
+# Initialize a counter variable
+counter = 1
+
+# Define the loop condition
+while counter <= 5:
+    # Print the current value of the counter
+    print(counter)
+
+    # Increment the counter
+    counter += 1
