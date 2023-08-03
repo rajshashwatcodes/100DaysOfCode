706f67c6a4fd2852379a7a841968223fd04230d2
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-08-03 23:32:35 +0530

    Create max_min_list.py

---
+++ Day_3/max_min_list.py
@@ -0,0 +1,11 @@
+# Given list of numbers:
+input_list = [10, 20, 30, 40, 50]
+print("List:", input_list)
+
+# Find the minimum and maximum elements in the list
+minimum = min(input_list)
+maximum = max(input_list)
+
+# Print the minimum and maximum elements in the list
+print("Minimum element:", minimum)
+print("Maximum element:", maximum)
