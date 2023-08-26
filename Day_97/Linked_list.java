0437f4d7948b3e6484ae73990e3ceeaa308f8226
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-08-26 23:46:53 +0530

    Create Linked_list.java

---
+++ Day_97/Linked_list.java
@@ -0,0 +1,49 @@
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.append(10);
+        list.append(20);
+        list.append(30);
+        list.append(40);
+
+        System.out.println("Linked List:");
+        list.display();
+    }
+}
