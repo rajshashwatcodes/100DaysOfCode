af05ed5289e1a20a49708870eee895db4b36a5e3
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-08-31 23:53:24 +0530

    Create BinarySearch2.java

---
+++ Day_105/BinarySearch2.java
@@ -0,0 +1,33 @@
+public class BinarySearch2 {
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid; // Element found at index mid
+            } else if (arr[mid] < target) {
+                left = mid + 1; // Search the right half
+            } else {
+                right = mid - 1; // Search the left half
+            }
+        }
+
+        return -1; // Element not found
+    }
+
+    public static void main(String[] args) {
+        int[] sortedArray = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+        int target = 11;
+
+        int result = binarySearch(sortedArray, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+}
