fa0f994b8aef2804e6bd45bd76129ce49a735795
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-09-08 23:29:24 +0530

    Create sort_array_reverse.py

---
+++ Day_11/sort_array_reverse.py
@@ -0,0 +1,8 @@
+# Input array
+arr = [5, 1, 4, 2, 8]
+
+# Sort the array in descending order
+arr_sorted = sorted(arr, reverse=True)
+
+# Print the sorted array
+print("Array in Descending Order:", arr_sorted)
