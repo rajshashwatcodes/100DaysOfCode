774aa33b8aba08fa28391fc55676e6557be564d7
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-09-21 23:32:24 +0530

    Create PalindromeLinkedList.java

---
+++ Day_9/PalindromeLinkedList.java
@@ -0,0 +1,63 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class PalindromeLinkedList {
+
+    public static boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true; // An empty list or a single element list is a palindrome.
+        }
+
+        // Find the middle of the linked list using the fast-slow pointer approach.
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse the second half of the linked list.
+        ListNode secondHalf = reverseLinkedList(slow);
+
+        // Compare the first half with the reversed second half.
+        while (secondHalf != null) {
+            if (head.val != secondHalf.val) {
+                return false; // Not a palindrome.
+            }
+            head = head.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true; // It is a palindrome.
+    }
+
+    private static ListNode reverseLinkedList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        // Create a sample linked list: 1 -> 2 -> 3 -> 2 -> 1
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(2);
+        head.next.next.next.next = new ListNode(1);
+
+        boolean result = isPalindrome(head);
+        System.out.println("Is the linked list a palindrome? " + result); // Output: true
+    }
+}
