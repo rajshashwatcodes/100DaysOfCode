d84d59fd4209aedd6be2581fd5069f4171470d16
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-10-19 23:54:03 +0530

    Create find_element_array.py

---
+++ Day_17/find_element_array.py
@@ -0,0 +1,11 @@
+# Input array
+arr = [64, 25, 12, 22, 11]
+
+# Element to search for
+element = 12
+
+# Check if the element exists in the array
+if element in arr:
+    print(f"{element} exists in the array.")
+else:
+    print(f"{element} does not exist in the array.")
